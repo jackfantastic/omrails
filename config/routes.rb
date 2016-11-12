c5a489467f806abb386577508f078c17589dd85c
Jack Fan <jackfanssj8@hotmail.com>
2016-11-12 19:53:59 +1100

    Styling Tweets

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
+  get 'users/:id', to: 'users#show', as: 'user'
+
   resources :dishes
   resources :tweets
   ActiveAdmin.routes(self)
   devise_for :users
   as :user do
-	get "sign_in" => 'devise/sessions#new'
-	delete "sign_out" => 'devise/sessions#destroy'
-	get "sign_up" => 'devise/registrations#new'  
+	get "sign_in", to: 'devise/sessions#new'
+	delete "sign_out", to: 'devise/sessions#destroy'
+	get "sign_up", to: 'devise/registrations#new'  
   end
   root 'pages#home'
-	get 'about' => 'pages#about'
-	get 'contact' => 'pages#contact'
+	get 'about', to: 'pages#about'
+	get 'contact', to: 'pages#contact'
+  get 'tweets', to: 'tweets#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
